fcab9b1948d06221363fe09253c356e99a4ca8a4
Geree <smithzero94@gmail.com>
2023-10-30 19:38:21 +0100

    first try

---
+++ src/main/java/hu/bankblaze/bankblaze/BankBlazeApplication.java
@@ -0,0 +1,13 @@
+package hu.bankblaze.bankblaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBlazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBlazeApplication.class, args);
+	}
+
+}
