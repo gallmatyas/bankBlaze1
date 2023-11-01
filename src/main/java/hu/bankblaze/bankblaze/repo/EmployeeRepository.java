fc6fa8fe5e20600be01a1777f6e60c57d39200d7
Ági <raczagi07@gmail.com>
2023-11-01 09:23:27 +0100

    employeeRepository good feature

--- src/main/java/hu/bankblaze/bankblaze/repo/EmployeeRepository.java
+++ src/main/java/hu/bankblaze/bankblaze/repo/EmployeeRepository.java
@@ -4,4 +4,5 @@ import hu.bankblaze.bankblaze.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
