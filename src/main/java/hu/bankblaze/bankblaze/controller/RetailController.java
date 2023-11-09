3b153aa9b33cd63749e54800ee53932906cef2a3
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-11-09 19:46:47 +0100

    ALL

--- src/main/java/hu/bankblaze/bankblaze/controller/RetailController.java
+++ src/main/java/hu/bankblaze/bankblaze/controller/RetailController.java
@@ -1,5 +1,7 @@
 package hu.bankblaze.bankblaze.controller;
 
+import hu.bankblaze.bankblaze.model.Retail;
+import hu.bankblaze.bankblaze.service.RetailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/hu/bankblaze/bankblaze/repo/TellerRepository.java
+++ src/main/java/hu/bankblaze/bankblaze/repo/TellerRepository.java
@@ -2,6 +2,8 @@ package hu.bankblaze.bankblaze.repo;
 
 import hu.bankblaze.bankblaze.model.Teller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TellerRepository extends JpaRepository<Teller, Long> {
 }
