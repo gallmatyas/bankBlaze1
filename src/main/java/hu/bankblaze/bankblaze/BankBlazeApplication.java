0ba94980c49c6b95bfd40ffd4da857634a08e22b
Geree <smithzero94@gmail.com>
2023-11-26 12:46:41 +0100

    debug css problem, little bit of code refinement done.

--- src/main/java/hu/bankblaze/bankblaze/BankBlazeApplication.java
+++ src/main/java/hu/bankblaze/bankblaze/BankBlazeApplication.java
@@ -2,19 +2,12 @@ package hu.bankblaze.bankblaze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BankBlazeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankBlazeApplication.class, args);
-
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		String encodedPassword = encoder.encode("jelszo");
-		System.out.println(encodedPassword);
-
 
 	}
 
