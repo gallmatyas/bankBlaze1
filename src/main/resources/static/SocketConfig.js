3c592b71f01b12f633daef4d0b60dddea7158140
Geree <smithzero94@gmail.com>
2023-12-03 08:41:31 +0100

    BB-115, queue call notification started

--- src/main/resources/static/SocketConfig.js
+++ src/main/resources/static/SocketConfig.js
@@ -1,19 +1,33 @@
-var stompClient = null;
+let stompClient = null;
+let socket = new SockJS('/bankBlaze-websocket');
+let id;
 
 function connect() {
-    var socket = new SockJS('/bankBlaze-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         stompClient.subscribe('/topic/app', function (desk) {
-            console.log("akarmi");
             sendRefresh(JSON.parse(desk.body));
+            clickOnIt();
         });
     });
+
 }
 
 function sendRefresh(desk) {
-    var id = "#queueNumber" + desk.id
-    console.log(desk.queueNumber.number);
-    console.log(id);
+    id = "#queueNumber" + desk.id;
     $(id).text(desk.queueNumber.number);
+}
+
+function clickOnIt() {
+    document.querySelector('#notifyButton').click();
+}
+
+function notify() {
+    let audio = document.getElementById('notify');
+    let promise = audio.play();
+    if (promise) {
+        promise.catch(function (error) {
+            console.error(error);
+        });
+    }
 }
